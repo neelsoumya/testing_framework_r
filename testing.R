36ab4439e8ef4979421edbaf0fc81e50490d277b
neelsoumya <neel.soumya@gmail.com>
2020-08-04 10:49:16 +0100

    R file

---
+++ testing.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(devtools)
+
+string <- "Testing is fun! hellooooo..."
+expect_match(string, "Testing") 
+expect_match(string, 'hello', ignore.case = TRUE)
+# expect_output(as.character(string), 'hello')
